00c0183bcff7f5f1fcf8f5eb93e9c034248f7ff7
sladuf <04fud27@gmail.com>
2024-06-26 21:32:42 +0900

    Test Toast Sample

---
+++ SwiftUI Toast Demo/SwiftUI Toast Demo/TestView.swift
@@ -0,0 +1,18 @@
+//
+//  TestView.swift
+//  SwiftUI Toast Demo
+//
+//  Created by Ryeong on 6/24/24.
+//
+
+import SwiftUI
+
+struct TestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TestView()
+}
